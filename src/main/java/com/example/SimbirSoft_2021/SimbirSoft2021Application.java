632b69cec84207158d8925b751d4dde4df0c0446
Швед Тимофей Юрьевич <timofeya123@inbox.ru>
2021-07-16 21:15:17 +0600

    start project

---
+++ src/main/java/com/example/SimbirSoft_2021/SimbirSoft2021Application.java
@@ -0,0 +1,13 @@
+package com.example.SimbirSoft_2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirSoft2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimbirSoft2021Application.class, args);
+	}
+
+}
