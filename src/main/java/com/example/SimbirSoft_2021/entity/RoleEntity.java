7acc54f05e23b7a8cf2f01ae4693cc400d02dc5c
Швед Тимофей Юрьевич <timofeya123@inbox.ru>
2021-07-26 22:57:12 +0600

    PostmanNewUrl

--- src/main/java/com/example/SimbirSoft_2021/entity/RoleEntity.java
+++ src/main/java/com/example/SimbirSoft_2021/entity/RoleEntity.java
@@ -1,7 +1,12 @@
 package com.example.SimbirSoft_2021.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
 @Table(name="role_entity")
 public class RoleEntity { // ----------------------------------------------- наш с вами пользователь
