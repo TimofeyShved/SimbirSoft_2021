027b13af82f0bdc3ad8af4c328944484f256587f
Швед Тимофей Юрьевич <timofeya123@inbox.ru>
2021-08-07 18:16:27 +0600

    TaskServiceDoc

--- src/main/java/com/example/SimbirSoft_2021/service/TaskService.java
+++ src/main/java/com/example/SimbirSoft_2021/service/TaskService.java
@@ -17,6 +17,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * <h1> Сервис задач - (TaskService) </h1>
+ * Данный класс реализует запросы, которые
+ * приходят в контроллер пользователей (TaskController),
+ * результат он возвращаяет обратно.
+ * <p>
+ * <b>Примечание:</b>
+ * В данном классе можно конструтор, организовать 3
+ * разными способами.
+ * А так же он использует свой интерфейс:
+ * вытащить все задачи по статусу (getAllByStatus),
+ * вытащить количество задач по статусу (getCountByStatus),
+ * поискать задачу по реализации (findByReleaseId),
+ * удалить задачу связанную с проектом (deleteTaskByProjectId),
+ * удалить реализацию связанную с проектом (deleteReleaseInTask).
+ * И стандартный для этого проекта, а это:
+ * регистрация (registration),
+ * вытащить всё (getAll),
+ * вытащить одно (getOne),
+ * удалить одно (deleteOne),
+ * обновить одно (updateOne).
+ *
+ * @автор  Швед Т.Ю.
+ * @версия 0.4
+ * @от   2021-08-13
+ */
+
 // 1 способ
 //@RequiredArgsConstructor
 @Service
@@ -39,10 +66,21 @@ public class TaskService implements StandartServiceInterface<TaskDto>, TaskServi
         this.roleService = roleService;
     }
 
-    // ----------------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * Это основной метод регистрации, из стандартного интерфейса
+     * использующий метод registration.
+     * Основная задача которой сохранить новой задачи в бд.
+     * @param taskDto Это первый и единственный параметр метода registration, который обозначает данные задачи.
+     * @return TaskDto Вернёт задачу.
+     * @throws ReleaseNotFoundException При ошибке если такая реализация даты/времени ещё не существует.
+     * @throws TaskAndDateTimeExistsException При ошибке если такая реализация даты/времени среди задач уже существует.
+     * @throws ProjectAndDateTimeExistsException При ошибке если такая реализация даты/времени среди проектов уже существует.
+     * @throws TaskExistsException При ошибке если такая реализация задачи уже существует.
+     * @throws StatusEnumException При ошибке если такой статус задачи не существует.
+     */
     @Transactional
-    @Override // ----------------- регистрация
-    public TaskDto registration(TaskDto taskDto) throws ReleaseExistsException, ReleaseNotFoundException, TaskAndDateTimeExistsException,
+    @Override
+    public TaskDto registration(TaskDto taskDto) throws ReleaseNotFoundException, TaskAndDateTimeExistsException,
             ProjectAndDateTimeExistsException, TaskExistsException, StatusEnumException {
 
         //  проверка
@@ -66,9 +104,16 @@ public class TaskService implements StandartServiceInterface<TaskDto>, TaskServi
         return TaskMapper.INSTANCE.toDto(taskEntity);
     }
 
-    // ----------------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * Это основной метод вытащить всё, из стандартного интерфейса
+     * использующий метод getAll.
+     * Основная задача которой вытащить все задачи из бд.
+     * @param () Не используется.
+     * @return List<TaskDto> Вернёт список задач.
+     * @throws TaskNotFoundException При ошибке если задач ещё не существует.
+     */
     @Transactional
-    @Override // ----------------- вытащить все задачи
+    @Override
     public List<TaskDto> getAll() throws TaskNotFoundException {
         List<TaskEntity> taskEntityList = taskCrud.findAll();
 
@@ -83,9 +128,16 @@ public class TaskService implements StandartServiceInterface<TaskDto>, TaskServi
         return taskDtoList;
     }
 
-    // ----------------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * Это основной метод вытащить одну задачу, из стандартного интерфейса
+     * использующий метод getOne.
+     * Основная задача которой вытащить одну задачу из бд.
+     * @param id Это первый и единственный параметр метода getOne, который обозначает номер задачи в бд.
+     * @return TaskDto Вернёт задачу.
+     * @throws TaskNotFoundException При ошибке если задач ещё не существует.
+     */
     @Transactional
-    @Override // ----------------- вытащить одну задачу
+    @Override
     public TaskDto getOne(Long id) throws TaskNotFoundException {
         TaskEntity taskEntity = taskCrud.findByTaskId(id);
 
@@ -97,9 +149,18 @@ public class TaskService implements StandartServiceInterface<TaskDto>, TaskServi
         return TaskMapper.INSTANCE.toDto(taskEntity);
     }
 
-    // ----------------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * Это дополнительный метод, что-бы
+     * вытащить все задачи по статусу и проекту, из личного интерфейса
+     * использующий метод getAllByStatus.
+     * Основная задача которой вытащить все задачи по статусу и проекту из бд.
+     * @param projectId Это первый параметр метода getAllByStatus, который обозначает номер проекта в бд.
+     * @param status Это второй параметр метода getAllByStatus, который обозначает статус проекта в бд.
+     * @return List<TaskDto> Вернёт список задач.
+     * @throws TaskNotFoundException При ошибке если задач ещё не существует.
+     */
     @Transactional
-    @Override // ----------------- вытащить задачи по статусу и проекту
+    @Override
     public List<TaskDto> getAllByStatus(Long projectId, String status) throws TaskNotFoundException {
         List<TaskEntity> taskEntityList = taskCrud.findAll();
 
@@ -122,9 +183,18 @@ public class TaskService implements StandartServiceInterface<TaskDto>, TaskServi
         return taskDtoList;
     }
 
-    // ----------------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * Это дополнительный метод, что-бы
+     * вытащить количество задач по статусу и проекту, из личного интерфейса
+     * использующий метод getCountByStatus.
+     * Основная задача которой вытащить количество задач по статусу и проекту из бд.
+     * @param projectId Это первый параметр метода getCountByStatus, который обозначает номер проекта в бд.
+     * @param status Это второй параметр метода getCountByStatus, который обозначает статус проекта в бд.
+     * @return Long Вернёт количество задач.
+     * @throws TaskNotFoundException При ошибке если задач ещё не существует.
+     */
     @Transactional
-    @Override // ----------------- вытащить количество задач по статусу и проекту
+    @Override
     public Long getCountByStatus(Long projectId, String status) throws TaskNotFoundException {
         List<TaskEntity> taskEntityList = taskCrud.findAll();
 
@@ -147,9 +217,16 @@ public class TaskService implements StandartServiceInterface<TaskDto>, TaskServi
         return countTask;
     }
 
-    // ----------------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * Это дополнительный метод, что-бы
+     * поискать задачу по реализации, из личного интерфейса
+     * использующий метод findByReleaseId.
+     * Основная задача которой поиск по реализации из бд.
+     * @param releaseId Это первый и единственный параметр метода findByReleaseId, который обозначает номер задачи в бд.
+     * @return TaskDto Вернёт задачу.
+     */
     @Transactional
-    @Override // --------------- поиск по реализации
+    @Override
     public TaskDto findByReleaseId(Long releaseId) {
         TaskEntity taskEntity = taskCrud.findByReleaseId(releaseId);
 
@@ -161,9 +238,18 @@ public class TaskService implements StandartServiceInterface<TaskDto>, TaskServi
         return TaskMapper.INSTANCE.toDto(taskEntity);
     }
 
-    // ----------------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * Это основной метод удалить одну задачу, из стандартного интерфейса
+     * использующий метод deleteOne.
+     * Основная задача которой удалить одну задачу из бд.
+     * @param id  Это первый и единственный параметр метода deleteOne, который обозначает номер задачи в бд.
+     * @return Long Вернёт номер задачи.
+     * @throws TaskNotFoundException При ошибке если задач ещё не существует.
+     * @throws ReleaseNotFoundException При ошибке если реализации даты/времени ещё не существует.
+     * @throws RoleNotFoundException При ошибке если ролей ещё не существует.
+     */
     @Transactional
-    @Override // ----------------- удалить одну задачу
+    @Override
     public Long deleteOne(Long id) throws TaskNotFoundException, ReleaseNotFoundException, RoleNotFoundException {
         TaskEntity taskEntity = taskCrud.findByTaskId(id);
 
@@ -182,9 +268,19 @@ public class TaskService implements StandartServiceInterface<TaskDto>, TaskServi
         return id;
     }
 
-    // ----------------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * Это дополнительный метод, что-бы
+     * удалить задачи связанные с проектом, из личного интерфейса
+     * использующий метод getCountByStatus.
+     * Основная задача которой удалить задачи связанные с проектом из бд.
+     * @param projectId Это первый и единственный параметр метода deleteTaskByProjectId, который обозначает номер проекта в бд.
+     * @return boolean Вернёт значение успеха выполения данного действия (логический).
+     * @throws TaskNotFoundException При ошибке если задач ещё не существует.
+     * @throws ReleaseNotFoundException При ошибке если реализации даты/времени ещё не существует.
+     * @throws RoleNotFoundException При ошибке если ролей ещё не существует.
+     */
     @Transactional
-    @Override // ----------------- удалить задачи связанные с проектом
+    @Override
     public boolean deleteTaskByProjectId(Long projectId) throws TaskNotFoundException, ReleaseNotFoundException, RoleNotFoundException {
         List<TaskEntity> taskEntityList = taskCrud.findAll();
 
@@ -203,9 +299,16 @@ public class TaskService implements StandartServiceInterface<TaskDto>, TaskServi
         return true;
     }
 
-    // ----------------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * Это дополнительный метод, что-бы
+     * удалить реализацию связанную с задачей, из личного интерфейса
+     * использующий метод deleteReleaseInTask.
+     * Основная задача которой удалить реализацию связанную с задачей из бд.
+     * @param id Это первый и единственный параметр метода deleteReleaseInTask, который обозначает номер реализация даты/времени в бд.
+     * @return boolean Вернёт значение успеха выполения данного действия (логический).
+     */
     @Transactional
-    @Override // ----------------- удалить реализацию связанную с задачей
+    @Override
     public boolean deleteReleaseInTask(Long id) {
         TaskEntity taskEntity = taskCrud.findByReleaseId(id);
         if(taskEntity!=null){
@@ -215,10 +318,23 @@ public class TaskService implements StandartServiceInterface<TaskDto>, TaskServi
         return true;
     }
 
-    // ----------------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * Это основной метод обновить одну задачу, из стандартного интерфейса
+     * использующий метод updateOne.
+     * Основная задача которой обновить одну задачу в бд.
+     * @param id Это первый параметр метода updateOne, который обозначает номер задачи в бд.
+     * @param taskDto Это второй параметр метода updateOne, который обозначает данные задачи.
+     * @return TaskDto Вернёт задачу.
+     * @throws TaskNotFoundException При ошибке если задач ещё не существует.
+     * @throws ReleaseNotFoundException При ошибке если реализации даты/времени ещё не существует.
+     * @throws TaskAndDateTimeExistsException При ошибке если такая реализация даты/времени среди задач уже существует.
+     * @throws ProjectAndDateTimeExistsException При ошибке если такая реализация даты/времени среди проектов уже существует.
+     * @throws TaskExistsException При ошибке если такая реализация задачи уже существует.
+     * @throws StatusEnumException При ошибке если такой статус задач не существует.
+     */
     @Transactional
-    @Override // ----------------- обновить одну задачу
-    public TaskDto updateOne(Long id, TaskDto taskDto) throws TaskNotFoundException, ReleaseExistsException, ReleaseNotFoundException,
+    @Override
+    public TaskDto updateOne(Long id, TaskDto taskDto) throws TaskNotFoundException, ReleaseNotFoundException,
             TaskAndDateTimeExistsException, ProjectAndDateTimeExistsException, TaskExistsException, StatusEnumException {
 
         //  проверка на то что задача вообще существуют
